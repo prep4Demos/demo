76b38c01e082ccf94917de5caec83b5a1b86fce0
archetana <archetana@users.noreply.github.com>
2019-04-03 17:40:10 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const URL_BASE = 'https://us-central1-synthetic-eon.cloudfunctions.net/';
+const SHEETS_URL = URL_BASE + 'sheets';
+
+let docs;
+
+fetch(SHEETS_URL).then(function(response) {
+  return response.json();
+}).then(function(responseJson) {
+  docs = responseJson;
+  const handsetNames = [...new Set(docs.map(doc => doc.handset))];
+  let ul = document.querySelector('tbody');
+  docs.forEach(doc => {
+    let tr = document.createElement("tr");
+    let name = document.createElement("td");
+    let phone = document.createElement("td");
+    let address = document.createElement("td");
+    name.appendChild(document.createTextNode(doc.name));
+    phone.appendChild(document.createTextNode(doc.number));
+    address.appendChild(document.createTextNode(doc.address));
+    tr.appendChild(name);
+    tr.appendChild(phone);
+    tr.appendChild(address);
+
+  })
+});
