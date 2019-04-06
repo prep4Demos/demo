7dd8946271040ca7657abdfa4c669e65e0dcd918
archetana <archetana@users.noreply.github.com>
2019-04-06 20:33:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
-    handleCors(req, res);
-    getRows().then(docs => {
+exports.sample = (req, res) => {
+  handleCORS(req,res)
+  getSheets().then(docs => {
         res.status(200).type('text/json').end(JSON.stringify(docs));
       })
-  };
-  
-  handleCors = (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
+};
+
+handleCORS = function(req,res){
+  res.set("Access-Control-Allow-Origin", "https://prep4demos.github.io");
   res.set("Access-Control-Allow-Methods", "GET");
   res.set("Access-Control-Allow-Headers", "Content-Type");
   res.set("Access-Control-Max-Age", "3600");
   if (req.method == 'OPTIONS') {
     res.status(204).send('');
    }
- }
- 
-const { google } = require('googleapis');
+}
+const {google} = require('googleapis');
 
-async function getRows() {
+async function getSheets(){
   let auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/spreadsheets']
   });
-  let api = google.sheets({version: 'v4', auth});
-  let response = await api.spreadsheets.values.get({
-    spreadsheetId: '1hfonsT_K11elbfSeA1p83IkEYDG1EwyHyIHt1mBJn2E',
-    range: 'A2:D'
+  let sheets = google.sheets({version: 'v4', auth});
+  let response = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1-rqyGcdhVZytu9Ve9Ht-e2bxNpZfFqufEfyLovGHRQw',
+    range: 'Form Responses 1!A2:C',
   });
   return response.data.values.map(row => {
     return {
-      name: row[1],
-      number: row[2],
-      address: row[3]
-    }
-  });
+      persona: row[1],
+      recommendation: row[2]
+    }})
 }
